c6948f7221f2e3c431ffe311bb54300418641525
jatinkumar13113-art <jatinkumar13113@gmail.com>
2026-01-29 00:15:58 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
+setTimeout(()=>{
+  document.getElementById("telegramPopup").style.display="flex";
+},3000);
 
+function closePopup(){
+  document.getElementById("telegramPopup").style.display="none";
+}
+
+function toggleMode(){
+  document.body.classList.toggle("light");
+}
+
+document.getElementById("search").addEventListener("keyup", function(){
+  let v=this.value.toLowerCase();
+  document.querySelectorAll(".card").forEach(c=>{
+    c.style.display=c.innerText.toLowerCase().includes(v)?"block":"none";
+  });
+});
