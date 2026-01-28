88b924ed914014b3da4e11aca21e3818020cccd4
jatinkumar13113-art <jatinkumar13113@gmail.com>
2026-01-28 08:44:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const toggle = document.getElementById("darkToggle");
 
-if (localStorage.getItem("dark") === "on") {
-  document.body.classList.add("dark");
-}
-
-if (toggle) {
-  toggle.onclick = () => {
-    document.body.classList.toggle("dark");
-    localStorage.setItem(
-      "dark",
-      document.body.classList.contains("dark") ? "on" : "off"
-    );
-  };
-}
