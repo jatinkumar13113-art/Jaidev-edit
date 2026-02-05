897714926f54096c93ea2eb47fcc22cf1d8c64df
jatinkumar13113-art <jatinkumar13113@gmail.com>
2026-02-05 22:37:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+function openSidebar() {
+  document.getElementById("sidebar").style.left = "0";
+  document.getElementById("overlay").style.display = "block";
+}
 
+function closeSidebar() {
+  document.getElementById("sidebar").style.left = "-260px";
+  document.getElementById("overlay").style.display = "none";
+}
+
+function showSection(id) {
+  document.querySelectorAll(".content").forEach(sec => sec.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
+  closeSidebar();
+}
