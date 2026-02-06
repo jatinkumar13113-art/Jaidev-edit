393c8c38473ce2a85dd495e722bbf1ad840c2cd8
jatinkumar13113-art <jatinkumar13113@gmail.com>
2026-02-06 19:08:29 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function openSidebar(){
-  document.querySelector('.sidebar').style.left='0';
-  document.getElementById('overlay').style.display='block';
-}
-function closeSidebar(){
-  document.querySelector('.sidebar').style.left='-260px';
-  document.getElementById('overlay').style.display='none';
-}
