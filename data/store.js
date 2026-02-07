83e0e8300a46907f5fb6080b5a5b014c7682874c
jatinkumar13113-art <jatinkumar13113@gmail.com>
2026-02-07 20:51:57 +0530

    Add files via upload

--- data/store.js
+++ data/store.js
@@ -1,1 +1,22 @@
+/* GLOBAL DATA STORE – SPCK SAFE */
+window.Store = (() => {
+  const KEY = "JAIDEV_TEMPLATES";
 
+  function get() {
+    try {
+      return JSON.parse(localStorage.getItem(KEY)) || [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  function set(data) {
+    localStorage.setItem(KEY, JSON.stringify(data));
+  }
+
+  return {
+    all: get,
+    add: (item) => set([...get(), item]),
+    remove: (id) => set(get().filter(t => t.id !== id))
+  };
+})();
